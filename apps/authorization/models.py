97052c205eb5a9a82d87e3e6cf8c67eeb73dddb2
Suvan <overmind77s@gmail.com>
2022-11-29 22:45:13 +0600

    add model customuser

--- apps/authorization/models.py
+++ apps/authorization/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as tr
 
-# Create your models here.
+from commons.constants import MENTOR, ADMIN, USER
+
+
+class CustomUser(AbstractUser):
+
+    USER_TYPE = (
+        (MENTOR, 'Mentor'),
+        (ADMIN, 'Admin'),
+        (USER, 'User')
+    )
+
+    is_mentor = models.BooleanField(
+        default=False,
+        help_text=tr(
+            'Поле для указания, является ли пользователь ментором'
+        )
+    )
+    user_type = models.CharField(
+        choices=USER_TYPE,
+        default=USER,
+        max_length=15
+    )
