740ef73299f2bae918fcc3ea0a8b7c29a1f5ad0d
Suvan <overmind77s@gmail.com>
2022-12-01 18:16:29 +0600

    fix models

--- apps/authorization/models.py
+++ apps/authorization/models.py
@@ -1,26 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as tr
+from django.utils.translation import gettext_lazy as _
 
+from apps.authorization.manager import CustomUserManager
 from commons.constants import MENTOR, ADMIN, USER
 
 
 class CustomUser(AbstractUser):
 
     USER_TYPE = (
-        (MENTOR, 'Mentor'),
-        (ADMIN, 'Admin'),
-        (USER, 'User')
+        (MENTOR, "Mentor"),
+        (ADMIN, "Admin"),
+        (USER, "User")
     )
+
     is_mentor = models.BooleanField(
         default=False,
-        help_text=tr(
-            'Поле для указания, является ли пользователь ментором'
+        hepl_text=_(
+            "Поле для указания, являеться ли пользователь ментором"
         )
     )
     user_type = models.CharField(
         choices=USER_TYPE,
         default=USER,
         max_length=15,
-        help_text=tr('Поле для обозначение типа пользователя')
+        hepl_text=_(
+            "Поле для обозначения типа пользователя"
+        )
     )
+    activation_code = models.CharField()
+
+    objects = CustomUserManager()
