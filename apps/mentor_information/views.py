21fccfd711817377b5f78370739f07978eef3b25
Suvan <overmind77s@gmail.com>
2022-12-01 18:38:09 +0600

    mentor information viev

--- apps/mentor_information/views.py
+++ apps/mentor_information/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from permissions.mentor import IsUserMentor
+from apps.mentor_information.models import MentorInformation
+from apps.mentor_information.serializers import MentorInformationSerializer
+
+
+class CreateMentorInformationView(CreateAPIView):
+    queryset = MentorInformation.objects.all()
+    serializer_class = MentorInformationSerializer
+    permission_classes = [IsUserMentor]
