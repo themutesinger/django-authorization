7bbfeec5c2962ab842c568900b0f001f1ab37e8a
Suvan <overmind77s@gmail.com>
2022-12-01 09:36:26 +0600

    admin creating service

--- apps/authorization/manager.py
+++ apps/authorization/manager.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.base_user import BaseUserManager
-
-from authorization.services.validating import validate_email
 from authorization.services.manager_servises import create_user
+from authorization.services.validating import validate_email
+jfrom django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
@@ -29,3 +28,25 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_admin(
+        self,
+        *,
+        last_name: str,
+        first_name: str,
+        password: str,
+        email: str,
+        **extra_field
+    ):
+        email_exists = validate_email(email=email)
+        if not email_exists:
+            raise ValueError('Пустой эмайл')
+        email = self.normalize_email(email=email)
+        user = create_admin(
+            last_name=last_name,
+            first_name=first_name,
+            email=email,
+            password=password,
+            **extra_field
+        )
+        user.save(using=self._db)
+        return user
--- apps/authorization/services/manager_servises.py
+++ apps/authorization/services/manager_servises.py
@@ -18,3 +18,22 @@ def create_user(
     user.set_password(password=password)
     return user
 
+def create_admin(
+    user_model,
+    *,
+    last_name: str,
+    first_name: str,
+    password: str,
+    email: str,
+    **extra_field
+):
+    user = user_model(
+        last_name=last_name,
+        email=email,
+        first_name=first_name,
+        **extra_field
+    )
+    user.set_password(password=password)
+    user.is_active = True
+    user.is_staff = True
+    return user
